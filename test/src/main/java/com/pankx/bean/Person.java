f1aaff5ff6a910e3016c36454fd4a15a6a072663
pkx0128 <764670547@qq.com>
2020-04-08 22:29:14 +0800

    yaml配置文件的基本用法

---
+++ test/src/main/java/com/pankx/bean/Person.java
@@ -0,0 +1,80 @@
+package com.pankx.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String name;
+    private Integer age;
+    private Date birthday;
+
+    private Map<String,Object> map;
+    private List<Object> list;
+
+    private Student student;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                ", map=" + map +
+                ", list=" + list +
+                ", student=" + student +
+                '}';
+    }
+}
--- test/src/test/java/com/pankx/TestApplicationTests.java
+++ test/src/test/java/com/pankx/TestApplicationTests.java
@@ -1,13 +1,22 @@
 package com.pankx;
 
+import com.pankx.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class TestApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
