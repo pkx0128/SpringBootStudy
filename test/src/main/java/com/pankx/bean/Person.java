d53fbb35f597f432a994c6b68b050a72c748f046
pkx0128 <764670547@qq.com>
2020-04-13 23:55:53 +0800

    添加JSR303测试

--- test/src/main/java/com/pankx/bean/Person.java
+++ test/src/main/java/com/pankx/bean/Person.java
@@ -1,21 +1,27 @@
 package com.pankx.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.Mapping;
 
+import javax.validation.constraints.Email;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "person")
+@Validated
 public class Person {
+//    @Value("${person.name}")
+    @Email
     private String name;
+//    @Value("${person.age}")
     private Integer age;
     private Date birthday;
-
     private Map<String,Object> map;
     private List<Object> list;
-
     private Student student;
 
     public String getName() {
